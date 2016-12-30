d95fc1ce3aa96ad8ad0c5444d7ab9421c1f5dacc
Chung-I <leon129506@gmail.com>
2016-12-30 11:59:25 +0800

    plot.py bug fixed

--- plot.py
+++ plot.py
@@ -5,8 +5,8 @@ import numpy as np
 step_size = 200
 
 
-def loss_dict_to_arr(stat_dict, step):
-  curr_step = step
+def loss_dict_to_arr(stat_dict, step_size):
+  curr_step = step_size
   stat_arr = []
   none_combo = 0
   while none_combo < 4:
@@ -16,15 +16,17 @@ def loss_dict_to_arr(stat_dict, step):
       none_combo = 0
     else:
       none_combo += 1
+    curr_step += step_size
   return stat_arr
 
 def mask_and_plot(np_arrs, labels, value_text, title_name, fig_name):
-  max_step = max([np_arr.size for np_arr in np_arrs])
-  x = np.arange(step_size, step_size * max_step + 1, step_size)
+  #max_step = max([np_arr.size for np_arr in np_arrs])
   plots = []
   for idx, np_arr in enumerate(np_arrs):
-    mask = np.isfinite(value_arr)
-    plots.append(plt.plot(x[mask],np_arr[mask],label=labels[idx]))
+    x = np.arange(step_size, step_size * np_arr.size + 1, step_size)
+    mask = np.isfinite(np_arr)
+    single_plot, = plt.plot(x[mask],np_arr[mask],label=labels[idx])
+    plots.append(single_plot)
   plt.legend(loc='upper left', handles=plots)
   plt.ylabel(value_text)
   plt.xlabel('step')
@@ -46,4 +48,4 @@ def jsons2arrs(file_names, value_name):
 value_arrs = jsons2arrs(sys.argv[3:], sys.argv[1])
 labels = [json_name.split(".")[:-1] for json_name in sys.argv[3:]]
 mask_and_plot(value_arrs, labels, sys.argv[1], sys.argv[1], sys.argv[2])
-      
+
